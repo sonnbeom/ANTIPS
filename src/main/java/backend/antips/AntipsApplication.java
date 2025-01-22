953a39e1d1ae6942ba494a88d8d88ce8104e679e
sonnbeom <dhsmf4245@gmail.com>
2025-01-22 12:50:54 +0900

    feat: 게이트웨이 라우터 설정

--- src/main/java/backend/antips/AntipsApplication.java
+++ src/main/java/backend/antips/AntipsApplication.java
@@ -3,6 +3,7 @@ package backend.antips;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class AntipsApplication {
 
