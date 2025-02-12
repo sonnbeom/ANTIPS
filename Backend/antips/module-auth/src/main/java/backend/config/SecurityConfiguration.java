080f00fb0f939b4d89897d6191f71df0ed096380
sonnbeom <dhsmf4245@gmail.com>
2025-02-12 14:36:44 +0900

    fix: modify docker file

--- Backend/antips/module-auth/src/main/java/backend/config/SecurityConfiguration.java
+++ Backend/antips/module-auth/src/main/java/backend/config/SecurityConfiguration.java
@@ -34,30 +34,30 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .cors((cors) -> cors
-                        .configurationSource(new CorsConfigurationSource() {
-                            @Override
-                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                                CorsConfiguration configuration = new CorsConfiguration();
-                                // 허용할 도메인
-                                configuration.setAllowedOrigins(Arrays.asList(
-                                        "http://localhost:3000",
-                                        "http://antips:3000",
-                                        "https://antips:3000"
-                                ));
-//                                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-                                // 허용할 메소드
-                                configuration.setAllowedMethods(Collections.singletonList("*"));
-                                // 쿠키나 인증 정보를 포함한 요청을 허용할지 여부 설정
-                                configuration.setAllowCredentials(true);
-                                // 모든 헤더 허용
-                                configuration.setAllowedHeaders(Collections.singletonList("*"));
-                                configuration.setExposedHeaders(Collections.singletonList("Authorization"));
-                                return configuration;
-
-                            }
-                        }));
+//        httpSecurity
+//                .cors((cors) -> cors
+//                        .configurationSource(new CorsConfigurationSource() {
+//                            @Override
+//                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+//                                CorsConfiguration configuration = new CorsConfiguration();
+//                                // 허용할 도메인
+//                                configuration.setAllowedOrigins(Arrays.asList(
+//                                        "http://localhost:3000",
+//                                        "http://antips:3000",
+//                                        "https://antips:3000"
+//                                ));
+////                                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+//                                // 허용할 메소드
+//                                configuration.setAllowedMethods(Collections.singletonList("*"));
+//                                // 쿠키나 인증 정보를 포함한 요청을 허용할지 여부 설정
+//                                configuration.setAllowCredentials(true);
+//                                // 모든 헤더 허용
+//                                configuration.setAllowedHeaders(Collections.singletonList("*"));
+//                                configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+//                                return configuration;
+//
+//                            }
+//                        }));
         httpSecurity
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
