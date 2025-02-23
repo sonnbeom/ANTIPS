2ac25d1da5b7c7d37138046e91f9c41883b9e605
sonnbeom <dhsmf4245@gmail.com>
2025-02-23 16:37:30 +0900

    remove: unnecessary file

--- Backend/antips/module-auth/src/main/java/backend/controller/AuthController.java
+++ Backend/antips/module-auth/src/main/java/backend/controller/AuthController.java
@@ -76,9 +76,5 @@ public class AuthController {
                 .data(authResponse)
                 .build();
     }
-    @GetMapping("/private")
-    public int welcome(){
-        return 200;
-    }
 
 }
